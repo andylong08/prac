fe0eaea07e5bde07f714cde766bfd8d70bd37552
andylong08 <andyllong08@gmail.com>
2025-03-27 23:55:01 -0500

    updated

--- node.js
+++ node.js
@@ -21,33 +21,17 @@ for (let i = 0; i < n; i++) {
         
         if (command === 'FLIP') {
             if (parts[1] === 'ALL') {
-                let newLights = [];
-                for (let k = 0; k < lights.length; k++) {
-                    if (lights[k] === '1') {
-                        newLights[k] = '0';
-                    } else {
-                        newLights[k] = '1';
-                    }
-                }
-                lights = newLights;
+                lights = lights.map(light => light === '1' ? '0' : '1');
             } else {
                 const a = parseInt(parts[1]);
                 const b = parseInt(parts[2]);
                 for (let k = a; k < b; k++) {
-                    if (lights[k] === '1') {
-                        lights[k] = '0';
-                    } else {
-                        lights[k] = '1';
-                    }
+                    lights[k] = lights[k] === '1' ? '0' : '1';
                 }
             }
         } else if (command === 'ON') {
             if (parts[1] === 'ALL') {
-                let newLights = [];
-                for (let k = 0; k < lights.length; k++) {
-                    newLights[k] = '1';
-                }
-                lights = newLights;
+                lights = lights.map(() => '1');
             } else {
                 const a = parseInt(parts[1]);
                 const b = parseInt(parts[2]);
@@ -57,11 +41,7 @@ for (let i = 0; i < n; i++) {
             }
         } else if (command === 'OFF') {
             if (parts[1] === 'ALL') {
-                let newLights = [];
-                for (let k = 0; k < lights.length; k++) {
-                    newLights[k] = '0';
-                }
-                lights = newLights;
+                lights = lights.map(() => '0');
             } else {
                 const a = parseInt(parts[1]);
                 const b = parseInt(parts[2]);
@@ -76,6 +56,4 @@ for (let i = 0; i < n; i++) {
     results.push(lights.join(''));
 }
 
-for (let i = 0; i < results.length; i++) {
-    console.log(results[i]);
-}
+results.forEach(result => console.log(result));
